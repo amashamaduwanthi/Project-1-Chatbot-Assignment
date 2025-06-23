bcce4a25e49dd43fea0236e61709150bc2388313
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-06-23 10:55:33 +0530

    initialized a react project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ChatWindow from "./Components/ChatWindow.tsx";
+function App() {
+    return (
+        <div className="App">
+            <ChatWindow />
+
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Components/ChatWindow.tsx
@@ -0,0 +1,9 @@
+const ChatWindow = () => {
+    return (
+        <div className="chat-container">
+          Role based Chatbot
+        </div>
+    );
+};
+
+export default ChatWindow;
