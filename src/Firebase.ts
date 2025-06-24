3ffd00d4304db012d4c2ffebddbdf796d787b674
amashamaduwanthi <amashamaduwanthi123@gmail.com>
2025-06-24 15:36:21 +0530

    import getAuth from Firebase Auth for authentication setup

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyD3E2Gam9Zy1_xlPJ9WTMA2OiCL3Hysf5Q",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+export const auth = getAuth(app);
